b2784a065c579d797a461e7f771e92f67df8fd15
Anders Weijnitz <aweijnitz@sapient.com>
2017-03-16 13:16:24 +0100

    Added request throttling

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var appConf = require('../conf/appConfig.json');
 var express = require('express');
 var router = express.Router();
+var throttle = require("express-throttle");
+
 
 var log4js = require('log4js');
 log4js.configure('./conf/log4js.json');
@@ -16,7 +18,7 @@ var registerUser = require('./registerUser')(appConf, log4js);
 // Remember, in Express 4, '/' is the root under which this route is mounted, so does not
 // necessarily correspond to the absolute root of the domain.
 //
-router.get('/', function(req, res) {
+router.get('/', throttle({ "rate": "5/s" }), function(req, res) {
   logger.debug('Serving / --> index.hjs');
   res.render('index', { title: 'QR Code Generator' });
 });
@@ -25,15 +27,15 @@ router.get('/', function(req, res) {
 
 // Generate QR code with given content (passed as query param)
 //
-router.get('/qr', qrCodeGenerator);
+router.get('/qr', throttle({ "rate": "5/s" }), qrCodeGenerator);
 
 // Add user, but don't generate any QR code. Intended for scripted DB fill, etc.
 //
-router.post('/users/:name', addUser);
-router.get('/users/:name', getUser);
+router.post('/users/:name', throttle({ "rate": "5/s" }), addUser);
+router.get('/users/:name', throttle({ "rate": "5/s" }), getUser);
 
 // Register user and return QR code containing the activation key
 //
-router.post('/register/:name', registerUser);
+router.post('/register/:name', throttle({ "rate": "5/s" }), registerUser);
 
 module.exports = router;
