22862c7aaa5f683ac0f71b0aee51279670d5d08d
Anders Weijnitz <anders.weijnitz@gmail.com>
2017-03-20 22:12:28 +0100

    Activation works

--- routes/activateUser.js
+++ routes/activateUser.js
@@ -3,18 +3,22 @@ var isDefined = require('../lib/utils').isStrDefined;
 var handleReq = function (appConf, log4js) {
     var logger = log4js.getLogger("activateUserRoute");
 
-    var activateUser = require('../lib/activateUserDB')(appConf, log4js);
+    var activateUserDB = require('../lib/activateUserDB')(appConf, log4js);
 
     return function addUser(req, res) {
         var code = req.params.code;
-        !isDefined(name) ? res.status(400).json({error: 'Code not specified'}) :
-            activateUser(req.app.locals.db, code,
-                function activateUserCallback(err, code) {
+
+        if (isDefined(code)) {
+            activateUserDB(req.app.locals.db, code,
+                function activateUserCallback(err, ok) {
                     if (err)
                         res.status(500).json({error: 'Could not activate code!', cause: err});
                     else
                         res.json({msg: 'Activated ' + code});
                 });
+        } else {
+            res.status(400).json({error: 'Code not specified'});
+        }
     };
 };
 
